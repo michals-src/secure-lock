1ed92108617c377d70f551c2a41e10c0491f3bdf
michals-src <54778121+michals-src@users.noreply.github.com>
2022-01-08 17:37:50 +0100

    zmiany 08.01.2022

--- src/main.cpp
+++ src/main.cpp
@@ -19,33 +19,121 @@ IPAddress local_IP(192, 168, 8, 5);
 IPAddress gateway(192, 168, 8, 1);
 IPAddress mask(255, 255, 255, 0);
 
-/* Just a little test message.  Go to http://192.168.4.1 in a web browser
-   connected to this access point to see it.
-*/
-void handleRoot()
+unsigned long czas_pracy;
+unsigned char softap_stations_count_last;
+unsigned char softap_stations_count;
+bool led_ostatni_stan;
+
+enum DIODA_LED
+{
+  R_LED = D2,
+  G_LED = D3,
+  B_LED = D4
+};
+
+enum PRZEKAZNIK_PINY
+{
+  PRZEKAZNIK_C1 = D0,
+  PRZEKAZNIK_C2 = D1
+}
+
+void
+handleRoot()
+{
+  server.send(200, "text/html", "<h1>Centrala systemu wykrywania otwierania drzwi</h1>");
+}
+
+void handleDetector_drzwi_otwarte()
+{
+  server.send(200, "text/html", "<h1>Drzwi są otwarte, rozłączenie przełącznika</h1>");
+  digitialWrite(PRZEKAZNIK_C1, LOW);
+  digitialWrite(PRZEKAZNIK_C2, LOW);
+
+  digitialWrite(G_LED, HIGH);
+  digitialWrite(B_LED, HIGH);
+  digitialWrite(R_LED, LOW);
+}
+
+void handleDetector_drzwi_zamkniete()
 {
   server.send(200, "text/html", "<h1>You are connected</h1>");
+  digitialWrite(PRZEKAZNIK_C1, HIGH);
+  digitialWrite(PRZEKAZNIK_C2, HIGH);
+
+  digitialWrite(R_LED, HIGH);
+  digitialWrite(B_LED, HIGH);
+  digitialWrite(G_LED, LOW);
+}
+
+void ustawienia_pinow()
+{
+  pinMode(PRZEKAZNIK_C1, OUTPUT);
+  pinMode(PRZEKAZNIK_C2, OUTPUT);
+  pinMode(R_LED, OUTPUT);
+  pinMode(G_LED, OUTPUT);
+  pinMode(B_LED, OUTPUT);
+}
+
+void http_punktykoncowe()
+{
+  server.on("/drzwi_otwarte", handleDetector_drzwi_otwarte);
+  server.on("/drzwi_zamkniete", handleDetector_drzwi_zamkniete);
 }
 
 void setup()
 {
-  delay(1000);
+
+  ustawienia_pinow();
+  digitialWrite(R_LED, HIGH);
+  digitialWrite(B_LED, HIGH);
+  digitialWrite(G_LED, HIGH);
+
   Serial.begin(115200);
   Serial.println();
-  Serial.print("Configuring access point...");
-  /* You can remove the password parameter if you want the AP to be open. */
+  Serial.print("Konfiguracja punktu dostępu");
   WiFi.softAPConfig(local_IP, gateway, mask);
+
+  /**
+   * 
+   * @param ssid, nazwa punktu dostępu
+   * @param password, hasło dostępu
+   * @param kanał, ustawiony na 1
+   * @param ukryty, punktu dostępu nie jest publiczny
+   */
   WiFi.softAP(ssid, password, 1, true);
 
   IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
+  Serial.print("AP IP adres: ");
   Serial.println(myIP);
-  server.on("/", handleRoot);
+
+  http_punktykoncowe();
   server.begin();
+
   Serial.println("HTTP server started");
+  digitialWrite(B_LED, LOW);
+
+  czas_pracy = millis();
+  softap_stations_count_last = "0";
+  led_ostatni_stan = false;
 }
 
 void loop()
 {
+  if (millis() - czas_pracy < 10)
+    return;
+
+  softap_stations_count = wifi_softap_get_station_num();
+  if (softap_stations_count_last != softap_stations_count)
+  {
+    for (uint8_t i = 0; i < 7; i++)
+    {
+      digitalWrite(B_LED, led_ostatni_stan);
+      led_ostatni_stan = !led_ostatni_stan;
+      delay(50);
+    }
+  }
+
   server.handleClient();
+
+  czas_pracy = millis();
 }
